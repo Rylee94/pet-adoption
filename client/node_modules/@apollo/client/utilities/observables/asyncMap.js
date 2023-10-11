a2e2bfff57a913aba033bf4a6fe30d32cd16cef9
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-10-10 19:37:21 -0500

    update typedefs so server will start, move server.js to server folder

---
+++ client/node_modules/@apollo/client/utilities/observables/asyncMap.js
@@ -0,0 +1,35 @@
+import { Observable } from "./Observable.js";
+export function asyncMap(observable, mapFn, catchFn) {
+    return new Observable(function (observer) {
+        var promiseQueue = {
+            then: function (callback) {
+                return new Promise(function (resolve) { return resolve(callback()); });
+            },
+        };
+        function makeCallback(examiner, key) {
+            return function (arg) {
+                if (examiner) {
+                    var both = function () {
+                        return observer.closed
+                            ? 0
+                            : examiner(arg);
+                    };
+                    promiseQueue = promiseQueue.then(both, both).then(function (result) { return observer.next(result); }, function (error) { return observer.error(error); });
+                }
+                else {
+                    observer[key](arg);
+                }
+            };
+        }
+        var handler = {
+            next: makeCallback(mapFn, "next"),
+            error: makeCallback(catchFn, "error"),
+            complete: function () {
+                promiseQueue.then(function () { return observer.complete(); });
+            },
+        };
+        var sub = observable.subscribe(handler);
+        return function () { return sub.unsubscribe(); };
+    });
+}
+//# sourceMappingURL=asyncMap.js.map
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,6 @@
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+module.exports = typeDefs;
