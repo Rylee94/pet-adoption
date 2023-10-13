87dc3184d9d4747980ef962d19f18326c58f9cce
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-10-12 19:29:13 -0500

    finish typeDefs, start resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,52 @@
-const { Profile } = require("../models");
+const { Profile, PetProfile } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    profiles: async () => {
+      return Profile.find();
+    },
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const profile = await Profile.findOne({ _id: context.user._id });
+        return profile;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    petProfiles: async () => {
+      return PetProfile.find();
+    },
+    petProfile: async (parent, { petProfileId }) => {
+      return PetProfile.findOne({ _id: petProfileId });
+    },
+  },
+
+  Mutation: {
+    addProfile: async (parent, { name, email, password, location }) => {
+      const profile = await Profile.create({ name, email, password, location });
+      const token = signToken(profile);
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    }
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,11 +6,60 @@ const typeDefs = `#graphql
         password: String
         location: String
     }
-    type User {
+    type PetProfile {
         _id: ID
-        
+        petName: String
+        petType: String
+        breed: String
+        age: String
+        gender: String
+        photo: String
+        aboutPet: String
+        pottyTrained: String
     }
     
+    type Auth {
+    token: ID!
+    user: Profile
+  }
+
+  type Query {
+      profiles: [Profile]
+      profile(profileId: ID!): Profile
+      me: Profile
+
+      petProfiles: [PetProfile]
+      petProfile(petProfileId: ID!): PetProfile
+      searchPets(petType: String, breed: String, location: String): [PetProfile]!
+
+
+  }
+
+  type Mutation {
+    addProfile(name: String!, email: String!, password: String!, location: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile: Profile
+    removeSkill(skill: String!): Profile
+
+    addPetProfile(petInput: PetInput!): PetProfile
+    updatePetProfile(petId: ID!, petInput: PetInput!): PetProfile
+    deletePetProfile(petId: ID!): PetProfile
+
+    updateUserProfile(name: String, email: String, password: String, location: String): Profile
+  }
+
+  input PetInput {
+    petName: String!
+    petType: String!
+    breed: String!
+    age: String!
+    gender: String!
+    photo: String
+    aboutPet: String!
+    pottyTrained: String
+  }
     `;
 
 module.exports = typeDefs;
