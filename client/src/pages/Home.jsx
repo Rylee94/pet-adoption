7f048f882ee5a65996f31bf46621d2e92af33e73
Ben Currier <benjamin.currier1@gmail.com>
2023-10-14 15:25:43 -0500

    updated the footer text, embedded youtube video, but its not running

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 export default function Home() {
+
     return (
         <>
             <div className="about-container">
@@ -8,10 +9,11 @@ export default function Home() {
                 <div className="about">
 
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem dolores, possimus, non in molestias reprehenderit unde nam at quam consequatur placeat, molestiae quos quia accusamus maiores? Fugit, quae explicabo.
-
-
+                        Who will you chose and remember for a lifetime?
                     </p>
+                    <div>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/nSz16ngdsG0?si=U7FMArzcLfMjoFh7&amp;clip=UgkxD-nPzt4kYchlqZebVAogOFS2G-Hc0F5L&amp;clipt=EAAY6IQB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </>
