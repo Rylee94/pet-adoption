bab64728e3c538a750f5c28a8c49474f7d74dcaf
Ben Currier <benjamin.currier1@gmail.com>
2023-10-17 21:16:46 -0500

    got the homepage music to work, with on off buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useEffect, useState} from 'react';
 import hero from '/images/rescue-pets.jpeg'
 // import TextField from '@mui/material/TextField';
 // import Button from '@mui/material/Button';
@@ -12,6 +11,25 @@ const defaultTheme = createTheme();
 
 
 const Home = () => {
+const [audio] = useState(new Audio('/music/homepagemusic.mp3'));
+const [isPlaying, setIsPlaying] = useState(true);
+
+useEffect(() => {
+  isPlaying ? audio.play() : audio.pause();
+}, [isPlaying, audio]);
+
+const handlePlay = () => {
+  setIsPlaying(true);
+  audio.play().catch((error) => console.error('Audio playback failed', error));
+};
+  
+const handlePause = () => {
+    setIsPlaying(false);
+};
+  
+  useEffect(() => {
+    audio.volume = 0.2;
+  }, [audio]);
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className="about-container" style={{ textAlign: 'center' }}>
@@ -29,6 +47,11 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <div style={{ position: 'relative', bottom: '-100px', right: '-1000px'}}>
+        <button onClick={handlePlay}>Start the music!</button>
+        <button onClick={handlePause}>Stop the music!</button>
+      </div>
+
     </ThemeProvider>
   );
 };
