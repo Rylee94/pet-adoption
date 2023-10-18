94626096d403e27b7b343f6e212cfef7d7c80d0c
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-10-17 21:51:32 -0500

    try fixing profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,23 @@
-export default function Profile() {
-    return <div>Profile</div>;
-}
+import React from 'react';
+
+import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
+
+import { ADD_PET } from '../utils/mutations';
+
+
+
+
+// import React, { useState } from 'react';
+// // import Pets from './Pets';
+
+// const Profile = ({ savedPets }) => {
+//   return (
+//     <div>
+//       <h2>Profile</h2>
+//       <h3>Saved Pets:</h3>
+//     </div>
+//   );
+// };
+
+// export default Profile;
