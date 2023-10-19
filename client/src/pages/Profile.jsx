b4c3a105db9a8b2b16fb56f698dbb1b16bb35cac
Ben Currier <benjamin.currier1@gmail.com>
2023-10-19 15:40:39 -0500

    updated readme, added some starting code for Profile page, modified sizing on Pets images code

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+
 export default function Profile() {
     return (
         <div>
             <h1>Profile Page</h1>
+            {/* <p>Name { me }</p> */}
         </div>
     );
 }
